35facebc39d02b94740b066e6bb1547c1fae3924
Andres-Chaparro-Diaz <72040539+Andres-Chaparro-Diaz@users.noreply.github.com>
2022-02-17 15:02:47 +0100

    Implementado los Rankings

--- TFG/userController.js
+++ TFG/userController.js
@@ -46,39 +46,99 @@ function userController() {
         createEventsLogin() {
             //falta controlar excepciones 
             let btnLogin = document.getElementById("entrar");
-            const event = new Event('click');
-            let tbUserName = document.getElementById("tbUsuario");
-            let tbPwd = document.getElementById("tbPwd");
 
             let controller = this;
             btnLogin.addEventListener('click', function(e) {
-                controller.login(tbUserName.textContent, tbPwd.textContent);
+                controller.login();
             }, false);
             //btnLogin.dispatchEvent(event);
         }
+
         createEventsRegister() {
-            let btnLogin = document.getElementById("btnCrearCuenta");
-            const event = new Event('click');
+            let btnRegister = document.getElementById("btnCrearCuenta");
+
+            let controller = this;
+            btnRegister.addEventListener('click', function(e) { controller.newUser() }, false);
+            //btnRegister.dispatchEvent(event);
+        }
+
+        newUser() {
+            let error = document.getElementById("error");
+            let message = document.getElementById("message");
             let tbUserName = document.getElementById("tbUsuario");
             let tbPwd = document.getElementById("tbPwd");
             let tbPwd2 = document.getElementById("tbPwd2");
 
-            let controller = this;
-            btnLogin.addEventListener('click', function(e) { controller.newUser(tbUserName.textContent, tbPwd.textContent, tbPwd2.textContent) }, false);
-            btnLogin.dispatchEvent(event);
-        }
-        newUser(userName, pwd, pwd2) {
             let users = this.readJSON();
-            if ("comprobar si existe en la bbdd" == "") {
-                user = new User(userName, pwd);
-                userJSON = { "id143": { "userName": userName, "password": password, "topPosiciones": [] } };
-                users = push(userJSON);
-                this.writeJSON(users)
+            let userName = tbUserName.value;
+            let pwd = tbPwd.value;
+            let pwd2 = tbPwd2.value;
+
+            let exist = false;
+            if (userName == "" || pwd == "" || pwd2 == "") {
+                error.textContent = "Rellene los campos";
+                return;
+            }
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].userName.toLowerCase() == userName.toLowerCase()) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (!exist) {
+                if (pwd == pwd2) {
+                    let userJSON = { "userName": userName, "password": pwd, "topPosiciones": [] };
+                    users.push(userJSON);
+                    this.writeJSON(users);
+                    message.textContent = "El usuario ha sido registrado";
+                    error.textContent = "";
+
+                } else {
+                    message.textContent = "";
+                    error.textContent = "Las contraseñas no coinciden";
+                }
+
             } else {
-                let error = document.getElementById("error");
+                message.textContent = "";
                 error.textContent = "El usuario ya existe";
             }
         }
+
+        login() {
+            let error = document.getElementById("error");
+            let message = document.getElementById("message");
+            let tbUserName = document.getElementById("tbUsuario");
+            let tbPwd = document.getElementById("tbPwd");
+
+            let users = this.readJSON();
+            let userName = tbUserName.value;
+            let password = tbPwd.value;
+
+            var found = false;
+
+            if (userName != "") {
+                for (var i = 0; i < users.length; i++) {
+                    if (users[i].userName.toLowerCase() == userName.toLowerCase()) {
+                        found = true;
+                        if (users[i].password == password) {
+                            error.textContent = "";
+                            message.textContent = "Credenciales válidas"
+                        } else {
+                            message.textContent = "";
+                            error.textContent = "Contraseña incorrecta";
+                        }
+                    }
+                }
+                if (!found) {
+                    message.textContent = "";
+                    error.textContent = "Usuario no encontrado";
+                }
+            } else {
+                message.textContent = "";
+                error.textContent = "Rellene los campos";
+            }
+        }
+
         changePassword(password, newPassword, userName) {
             let users = this.readJSON();
             if ("existe el usuario en la bbdd" == userName) {
@@ -91,11 +151,81 @@ function userController() {
                 error.textContent = "El usuario no existe";
             }
         }
+        loadPersonalRank() {
+            let userLogged = sessionStorage.getItem("userName");
+            if (userLogged != "" || userLogged != null) {
+                let users = this.readJSON();
+                let ulPP = document.getElementById("ulPersonalPoints");
+                for (var i = 0; i < users.length; i++) {
+                    if (users[i].userName.toLowerCase() == userLogged.toLowerCase()) {
+                        found = true;
+                        let pointList = users[i].topPuntuaciones;
+                        pointList.sort(function(a, b) { return b - a });
+                        for (var j = 0; j < pointList.length; j++) {
+                            let li = document.createElement("li");
+                            let div = document.createElement("div");
+                            let div1 = document.createElement("div");
+                            let p = document.createElement("p");
+                            p.value = pointList[j];
+                            p.textContent = pointList[j];
+                            div.className = "liDiv"; //esto falta utilizarlo y ponerle los estilos
+                            div1.appendChild(p);
+                            div.appendChild(div1);
+                            li.appendChild(div)
+                            ulPP.appendChild(li);
+                        }
+                    }
+                }
+            }
+        }
 
-        login(userName, password) {
+        loadGlobalRank() {
             let users = this.readJSON();
-            let message = document.getElementById("message");
-            message.textContent = "El usuario no existe";
+            let ulGP = document.getElementById("ulGlobalPoints");
+            let ulGU = document.getElementById("ulGlobalUsers");
+            let globalListPoints = [];
+            let globalListUsers = [];
+            for (var i = 0; i < users.length; i++) {
+                let personalList = users[i].topPuntuaciones;
+                personalList.sort(function(a, b) { return b - a });
+                globalListPoints.push(personalList[0]);
+                let user = { "userName": users[i].userName, "points": personalList[0] };
+                globalListUsers.push(user);
+            }
+            globalListPoints.sort(function(a, b) { return b - a });
+            for (var i = 0; i < globalListPoints.length; i++) {
+                let li = document.createElement("li");
+                let div = document.createElement("div");
+                let div1 = document.createElement("div");
+                let p = document.createElement("p");
+                p.value = globalListPoints[i];
+                p.textContent = globalListPoints[i];
+                div.className = "liDiv"; //esto falta utilizarlo y ponerle los estilos
+                div1.appendChild(p);
+                div.appendChild(div1);
+                li.appendChild(div)
+                ulGP.appendChild(li);
+                for (var j = 0; j < globalListUsers.length; j++) {
+                    // si hay 2 puntuaciones iguales la liamos aqui eh
+                    if (globalListUsers[j].points != 0) {
+                        if (globalListPoints[i] == globalListUsers[j].points) {
+                            let liU = document.createElement("li");
+                            let divU = document.createElement("div");
+                            let divU1 = document.createElement("div");
+                            let pU = document.createElement("p");
+                            pU.value = globalListUsers[j].userName;
+                            pU.textContent = globalListUsers[j].userName;
+                            globalListUsers[j].userName = "";
+                            globalListUsers[j].points = 0;
+                            div.className = "liDiv"; //esto falta utilizarlo y ponerle los estilos
+                            divU1.appendChild(pU);
+                            divU.appendChild(divU1);
+                            liU.appendChild(divU)
+                            ulGU.appendChild(liU);
+                        }
+                    }
+                }
+            }
         }
 
         readJSON() {
@@ -113,14 +243,21 @@ function userController() {
         writeJSON(users) {
             const requestURL = 'http://localhost/phaser/tutorial/TFG/users.json';
             const request = new XMLHttpRequest();
-            request.open('POST', requestURL);
-            request.responseType = 'json';
+            request.open('POST', requestURL, false);
+            request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
             request.send(JSON.stringify(users));
         }
     }
     let control = new controller();
-    control.createEventsLogin();
-    control.createEventsRegister();
+    if (document.location.href == 'http://localhost/phaser/tutorial/TFG/register.html') {
+        control.createEventsRegister();
+
+    } else if (document.location.href == 'http://localhost/phaser/tutorial/TFG/login.html') {
+        control.createEventsLogin();
+    } else if (document.location.href == 'http://localhost/phaser/tutorial/TFG/ranking.html') {
+        control.loadGlobalRank();
+        control.loadPersonalRank();
+    }
 }
 
 userController()
